59082d01767142f78df1c130234deb87fe4223d2
Afiya21 <NebyatAhmed21@gmail.com>
2021-08-31 22:03:16 +0300

    creating new file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - entry point.
+ *Description: program starts and ends here.
+ *Return: 0 on success.
+ */
+int main(void)
+{
+char *j = "Holberton";
+int i;
+for (i = 0; i <= 8; i++)
+{
+_putchar(j[i]);
+}
+_putchar('\n');
+return (0);
+}
+
