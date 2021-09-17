a4fc7458d7fbd881ed75a0537c7c9b9ef12afd94
Afiya21 <NebyatAhmed21@gmail.com>
2021-09-17 23:28:09 +0300

    this is

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,20 @@
-#include <stdio.h>  
-void main(int argc, char *argv[] )  {  
-  
-   printf("Program name is: %s\n", argv[0]);  
-   
-   if(argc < 2){  
-      printf("No argument passed through command line.\n");  
-   }  
-   else{  
-      printf("First argument is: %s\n", argv[1]);  
-   }  
-}  
+#include <stdio.h>
+/**
+  *main - prints the number of arguments passed to it.
+  *@argc: number of arguemnts.
+  *@argv: array of size argc.
+  *
+  *Return: 0 on success.
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+	(void)argv;
+
+	for (i = 0; i < argc - 1; i++)
+	{}
+
+	printf("%d\n", i);
+	return (0);
+}
+
